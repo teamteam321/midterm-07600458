50528a1d491eb3e14994ccf9a2c0db36b814e850
teamteam321 <kooowin@gmail.com>
2020-09-06 10:28:40 +0700

    change: clear output field when click calculate

--- app/src/main/java/com/akzaki/speedcalculator/MainActivity.java
+++ app/src/main/java/com/akzaki/speedcalculator/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                out.setText("");
                 try{
                     double dis = Double.parseDouble(distance.getText().toString());
                     double tm = Double.parseDouble(second.getText().toString());
