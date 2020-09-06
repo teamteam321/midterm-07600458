04899fc8a9fa118ad76b2f6c7b26070699d7a15e
teamteam321 <kooowin@gmail.com>
2020-09-06 10:04:58 +0700

    main activity

--- app/src/main/java/com/akzaki/speedcalculator/MainActivity.java
+++ app/src/main/java/com/akzaki/speedcalculator/MainActivity.java
@@ -2,7 +2,12 @@ package com.akzaki.speedcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button clear = findViewById(R.id.clear_button);
+        final Button calculate = findViewById(R.id.calculate_button);
+        final TextView distance = findViewById(R.id.input_distance);
+        final TextView second = findViewById(R.id.input_second);
+        final TextView out = findViewById(R.id.output_speed);
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                distance.setText("");
+                second.setText("");
+            }
+        });
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    double dis = Double.parseDouble(distance.getText().toString());
+                    double tm = Double.parseDouble(second.getText().toString());
+                    if(tm == 0.0){
+                        Toast msg = Toast.makeText(MainActivity.this, R.string.time_greater_zero,Toast.LENGTH_LONG);
+                        msg.show();
+                        return;
+                    }
+                    dis /= 1000;
+                    tm /= 3600;
+                    double out_spd = dis/tm;
+                    out.setText(String.format("%.2f",out_spd));
+                    if(out_spd > 80.0){
+                        AlertDialog.Builder result_a = new AlertDialog.Builder(MainActivity.this);
+                        result_a.setTitle("SPEED CALCULATOR");
+                        result_a.setMessage(R.string.over_limit);
+                        result_a.setPositiveButton("OK",null);
+                        result_a.show();
+                    }
+                }catch(Exception e){
+                    Toast msg = Toast.makeText(MainActivity.this, R.string.distance_time_required,Toast.LENGTH_LONG);
+                    msg.show();
+                    return;
+                }
+
+            }
+        });
+
     }
 }
